e8000761ce559999671fc0b0d7bb3a9937be7461
Dave Jensen <davidj@nextravel.com>
2021-04-19 21:21:52 -0700

    change url based on environment

--- src/recipes/context/RecipeContext.tsx
+++ src/recipes/context/RecipeContext.tsx
@@ -1,12 +1,14 @@
 import React, { useContext } from 'react';
 import { RecipeBloc } from '../blocs/RecipeBloc';
 import { Configuration, RecipesApi } from '../client';
+import process from 'process';
 
 
+const basePath = process.env.NODE_ENV === "test" ? "http://localhost" : "http://localhost:8000";
 const RecipeContext = React.createContext<RecipeBloc | null>(null);
 
 export const RecipeContextProvider: React.FC = ({children}) => {
-  const recipesApi = new RecipesApi(new Configuration({ basePath: "http://localhost:8000" }));
+  const recipesApi = new RecipesApi(new Configuration({ basePath: basePath }));
   const recipeBloc = new RecipeBloc(recipesApi);
   return <RecipeContext.Provider value={recipeBloc}>{children}</RecipeContext.Provider>
 }
--- src/recipes/context/RecipeListContext.tsx
+++ src/recipes/context/RecipeListContext.tsx
@@ -3,11 +3,12 @@ import { RecipeListBloc } from '../blocs/RecipeListBloc';
 import { Configuration, RecipesApi } from '../client';
 
 
+const basePath = process.env.NODE_ENV === "test" ? "http://localhost" : "http://localhost:8000";
 const RecipeListContext = React.createContext<RecipeListBloc | null>(null);
 
 export const RecipeListContextProvider: React.FC = ({children}) => {
   // nice to have: dependency injection on this
-  const recipesApi = new RecipesApi(new Configuration({ basePath: "http://localhost:8000" }));
+  const recipesApi = new RecipesApi(new Configuration({ basePath: basePath }));
   const recipeListBloc = new RecipeListBloc(recipesApi);
   return <RecipeListContext.Provider value={recipeListBloc}>{children}</RecipeListContext.Provider>
 }
