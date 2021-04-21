5289c1d71078812cc9bfab32414314504f133df4
Dave Jensen <davidj@nextravel.com>
2021-04-20 23:35:09 -0700

    add a few tests

--- src/recipes/components/__tests__/RecipeList.test.tsx
+++ src/recipes/components/__tests__/RecipeList.test.tsx
@@ -1,11 +1,20 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { findAllByRole, findByText, fireEvent, getByLabelText, getByRole, render, screen } from "@testing-library/react";
 import { recipeTestData, setupTestServer } from "../../__mocks__/server";
 import { RecipesList } from "../RecipesList";
 import { RecipeListContextProvider } from "../../context/RecipeListContext";
 import { RecipeContextProvider } from "../../context/RecipeContext";
 import { grommet, Grommet } from "grommet";
 
+const recipeList = (
+  <Grommet theme={grommet} themeMode="light" full>
+    <RecipeListContextProvider>
+      <RecipeContextProvider>
+        <RecipesList />
+      </RecipeContextProvider>
+    </RecipeListContextProvider>
+  </Grommet>
+);
 
 describe("RecipeList (integration test)", () => {
   const testServer = setupTestServer();
@@ -14,22 +23,48 @@ describe("RecipeList (integration test)", () => {
   afterEach(() => testServer.resetHandlers());
   afterAll(() => testServer.close());
 
-  it("should render RecipeList component", async () => {
-    render(
-      <Grommet theme={grommet} themeMode="light" full>
-        <RecipeListContextProvider>
-          <RecipeContextProvider>
-            <RecipesList />
-          </RecipeContextProvider>
-        </RecipeListContextProvider>
-      </Grommet>
-    );
+  test("should render RecipeList component", async () => {
+    render(recipeList);
 
     const items = await screen.findAllByRole("listitem");
-    expect(items).toHaveLength(3);
+    expect(items).toHaveLength(recipeTestData.length);
     for (const recipe of recipeTestData) {
       let elem = await screen.findByText(recipe.name);
       expect(elem).toBeInTheDocument();
     }
   });
+
+  test("should show ingredients on click", async () => {
+    render(recipeList);
+
+    const items = await screen.findAllByRole("listitem");
+    expect(items).toHaveLength(recipeTestData.length);
+
+    fireEvent.mouseEnter(items[0]);
+    fireEvent.click(items[0]);
+
+    const ingredients = await findAllByRole(items[0], "listitem");
+    expect(ingredients).toHaveLength(recipeTestData[0].ingredients.length);
+    for (const ingredient of recipeTestData[0].ingredients) {
+      expect(await findByText(items[0], ingredient.name)).toBeInTheDocument();
+    }
+  });
+
+  test("should open delete dialog", async () => {
+    render(recipeList);
+
+    const items = await screen.findAllByRole("listitem");
+    expect(items).toHaveLength(3);
+
+    fireEvent.mouseEnter(items[0]);
+    fireEvent.click(getByRole(items[0], "button", {name: "delete"}));
+
+    const dialog = await screen.findByRole("dialog");
+    expect(dialog).toBeInTheDocument();
+
+    const text = await findByText(dialog, recipeTestData[0].name);
+    expect(text).toBeInTheDocument();
+  });
+
+  test.skip("should open edit dialog", () => {});
 });
